1072e76e368d3ba21ba21fe85afb72b241b058a8
sasha269209 <work269209@gmail.com>
2024-09-23 21:52:58 +0200

    Add new files

---
+++ 038.cpp
@@ -0,0 +1,16 @@
+//file 038.cpp   ( Protcec ...)
+//lessons 28 {@pathtocpp cpp}
+
+#include <iostream>
+#include <algorithm>
+#define PI 3.1415
+using std::cout;
+using std::endl;
+
+
+int main()
+{
+cout << PI << "\n";
+
+return 0;
+}
