54586c49e10ee3f3955b61f34b077274271539ee
sasha269209 <work269209@gmail.com>
2024-09-25 21:28:39 +0200

    Add new files

---
+++ 040.cpp
@@ -0,0 +1,32 @@
+//file 039.cpp   ( #define ...)
+//lessons 29 {@pathtocpp cpp}
+
+#include <iostream>
+#include <algorithm>
+
+using std::cout;
+using std::endl;
+
+
+int main()
+{
+int a=5, b=10, c;
+
+if (a<b)
+{
+    cout << "a less than b" << "\n";
+}
+else
+{
+    cout << "a more than b" << "\n";
+}
+
+
+a < b ? cout << "a less than b" << "\n" : cout << "a more than b" << "\n";
+
+
+c = (a < b) ? a:b;
+cout << c << "\n";
+
+return 0;
+}
