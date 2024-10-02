182c09fded33d6f5be3929a428b9752c56fd3577
sasha269209 <work269209@gmail.com>
2024-10-02 19:01:24 +0200

    Add new files

---
+++ 050.cpp
@@ -0,0 +1,48 @@
+// file 050.cpp   ( OOP Example..)
+// lessons 39 {@pathtocpp cpp}
+
+#include <iostream>
+#include <string>
+using std::cin;
+using std::cout;
+using std::string;
+
+class car
+{
+private:
+    bool eng_work;
+
+public:
+    car() {};
+    car(bool _eng_work)
+    {
+        eng_work = _eng_work;
+    }
+
+    void lest_go()
+    {
+        if (eng_work == 1)
+        {
+            cout << "OK we go " << "\n";
+        }
+        else
+        {
+            cout << "Engin is not work" << "\n";
+            cout << "you shout start engine" << "\n";
+        }
+    }
+
+    void start_eng(bool _eng_work)
+    {
+        eng_work = _eng_work;
+        (eng_work == true) ? cout << "engin start" << "\n" : cout << "engin stop" << "\n";
+    }
+};
+
+int main()
+{
+    car bmw;
+    bmw.lest_go();
+    bmw.start_eng(true);
+    bmw.lest_go();
+}
