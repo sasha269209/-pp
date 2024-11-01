c726b413cc246072b9de88ad10f4e134e56368e6
sasha269209 <work269209@gmail.com>
2024-11-01 20:39:27 +0100

    Add new files

---
+++ 069.cpp
@@ -0,0 +1,44 @@
+// file 069.cpp   (  Virtual Destructor..)
+// lessons 57 {@pathtocpp cpp}
+
+#include <iostream>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Parent
+{
+public:
+    Parent()
+    {
+        cout << "Constructor Parent" << "\n";
+    }
+    virtual ~Parent()
+    {
+        cout << "Destructor Parent" << "\n";
+    }
+};
+
+class Child : public Parent
+
+{
+public:
+    Child()
+    {
+        cout << "Constructor Child" << "\n";
+    }
+    ~Child()
+    {
+        cout << "Destructor Child" << "\n";
+    }
+};
+
+int main()
+{
+    // Child c;
+    Parent *ptr = new Child();
+
+    delete ptr;
+}
